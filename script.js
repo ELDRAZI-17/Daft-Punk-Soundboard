2d0cf1e241e4b25383824d73e1920f53bfbf92fb
ELDRAZI-17 <78639467+ELDRAZI-17@users.noreply.github.com>
2024-03-29 18:25:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,39 @@ function playAudio(soundId) {
   var audio = document.getElementById(soundId);
   audio.play();
 }
+
+var fullscreenBtn = document.getElementById('fullscreen-btn');
+
+fullscreenBtn.addEventListener('click', function() {
+    if (document.fullscreenElement || document.webkitFullscreenElement ||
+        document.mozFullScreenElement || document.msFullscreenElement) {
+        exitFullscreen();
+    } else {
+        requestFullscreen();
+    }
+});
+
+function requestFullscreen() {
+    var element = document.documentElement;
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
