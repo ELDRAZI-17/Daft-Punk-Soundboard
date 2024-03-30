eec0e1c22f204c20b95d1cbde6799a1121b4995b
ELDRAZI-17 <78639467+ELDRAZI-17@users.noreply.github.com>
2024-03-30 12:55:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//play audio
 function playAudio(soundId) {
   var audio = document.getElementById(soundId);
   audio.play();
@@ -11,6 +12,8 @@ function stopAllAudio() {
   });
 }
 
+
+//fullscreen
 var fullscreenBtn = document.getElementById('fullscreen-btn');
 
 fullscreenBtn.addEventListener('click', function() {
@@ -47,6 +50,8 @@ function exitFullscreen() {
     }
 }
 
+
+//keyboard
 document.addEventListener("keydown", function(event) {
   switch(event.key) {
 
@@ -127,3 +132,108 @@ document.addEventListener("keydown", function(event) {
       break;
   }
 });
+
+
+//MIDI Input
+document.addEventListener('DOMContentLoaded', function() {
+  if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess()
+      .then(onMIDISuccess, onMIDIFailure);
+  } else {
+    alert('Your browser does not support the Web MIDI API.');
+  }
+});
+
+function onMIDISuccess(midiAccess) {
+  var inputs = midiAccess.inputs;
+  inputs.forEach(function(input) {
+    input.onmidimessage = getMIDIMessage;
+  });
+}
+
+function onMIDIFailure() {
+  alert('Unable to access your MIDI device.');
+}
+
+function getMIDIMessage(message) {
+  var command = message.data[0];
+  var note = message.data[1];
+  var velocity = (message.data.length > 2) ? message.data[2] : 0;
+  
+  if (command === 144 && note === 60 && velocity > 0) {
+    playAudio('work_it');
+  }
+
+  if (command === 144 && note === 61 && velocity > 0) {
+    playAudio('harder');
+  }
+
+  if (command === 144 && note === 62 && velocity > 0) {
+    playAudio('make_it');
+  }
+
+  if (command === 144 && note === 63 && velocity > 0) {
+    playAudio('better');
+  }
+
+  if (command === 144 && note === 64 && velocity > 0) {
+    playAudio('do_it');
+  }
+
+  if (command === 144 && note === 65 && velocity > 0) {
+    playAudio('faster');
+  }
+
+  if (command === 144 && note === 66 && velocity > 0) {
+    playAudio('makes_us');
+  }
+
+  if (command === 144 && note === 67 && velocity > 0) {
+    playAudio('stronger');
+  }
+
+  if (command === 144 && note === 68 && velocity > 0) {
+    playAudio('more_than');
+  }
+
+  if (command === 144 && note === 69 && velocity > 0) {
+    playAudio('ever');
+  }
+
+  if (command === 144 && note === 70 && velocity > 0) {
+    playAudio('hour');
+  }
+
+  if (command === 144 && note === 71 && velocity > 0) {
+    playAudio('after');
+  }
+
+  if (command === 144 && note === 72 && velocity > 0) {
+    playAudio('our');
+  }
+
+  if (command === 144 && note === 73 && velocity > 0) {
+    playAudio('work_is');
+  }
+
+  if (command === 144 && note === 74 && velocity > 0) {
+    playAudio('never');
+  }
+
+  if (command === 144 && note === 75 && velocity > 0) {
+    playAudio('over');
+  }
+
+  if (command === 144 && note === 76 && velocity > 0) {
+    playAudio('all');
+  }
+
+  if (command === 144 && note === 77 && velocity > 0) {
+    playAudio('all_2');
+  }
+
+  if (command === 144 && note === 78 && velocity > 0) {
+    stopAllAudio();
+  }
+
+}
