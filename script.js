af79ee5b631d92caee247652ccba53489463ec45
ELDRAZI-17 <78639467+ELDRAZI-17@users.noreply.github.com>
2024-03-30 09:41:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ function playAudio(soundId) {
   audio.play();
 }
 
-function refreshPage() {
-    location.reload();
+function stopAllAudio() {
+  var allAudio = document.querySelectorAll('audio');
+  allAudio.forEach(function(audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  });
 }
 
 var fullscreenBtn = document.getElementById('fullscreen-btn');
@@ -42,3 +46,80 @@ function exitFullscreen() {
         document.msExitFullscreen();
     }
 }
+
+document.addEventListener("keydown", function(event) {
+  switch(event.key) {
+
+    case "a":
+      playAudio('work_it');
+      break;
+
+    case "q":
+      playAudio('harder');
+      break;
+
+    case "z":
+      playAudio('make_it');
+      break;
+
+    case "s":
+      playAudio('better');
+      break;
+
+    case "e":
+      playAudio('do_it');
+      break;
+
+    case "d":
+      playAudio('faster');
+      break;
+
+    case "r":
+      playAudio('makes_us');
+      break;
+
+    case "f":
+      playAudio('stronger');
+      break;
+
+    case "t":
+      playAudio('more_than');
+      break;
+
+    case "g":
+      playAudio('ever');
+      break;
+
+    case "y":
+      playAudio('hour');
+      break;
+
+    case "h":
+      playAudio('after');
+      break;
+
+    case "u":
+      playAudio('our');
+      break;
+
+    case "j":
+      playAudio('work_is');
+      break;
+
+    case "i":
+      playAudio('never');
+      break;
+
+    case "k":
+      playAudio('over');
+      break;
+
+    case "w":
+      playAudio('all');
+      break;
+
+    case "x":
+      playAudio('all_2');
+      break;
+  }
+});
